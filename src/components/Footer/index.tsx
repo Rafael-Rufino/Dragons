7e527fd98891e0c6da9d450f932240e710b64557
RafaelRufino <rafaelrufino.52@gmail.com>
2022-01-25 01:11:42 -0300

    componente de rodape

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "../../styles/footer.scss";
+export default function Footer() {
+  return (
+    <footer id="rodape">
+      <strong>Study app developed with 🧡 by @RafaelRufino</strong>
+    </footer>
+  );
+}
