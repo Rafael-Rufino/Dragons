e80247ee9284d5ec176ae70579dc8bbb2acc4335
RafaelRufino <rafaelrufino.52@gmail.com>
2022-01-25 13:33:00 -0300

    ajustes importante na aplicação

--- src/contexts/user-context-provider.js
+++ src/contexts/user-context-provider.js
@@ -5,8 +5,9 @@ const IsLoginPage = () => window.location.href.indexOf("login/") !== -1;
 
 function UserContextProvider({ children }) {
   let history = useHistory();
+  //const [user, setUser] = useState(null);
 
-  const [user, setUser] = useState(null, () => {
+  const [user, setUser] = useState(() => {
     const storagedDragons = localStorage.getItem("@GithubDragons:user");
     if (storagedDragons) {
       return JSON.parse(storagedDragons);
