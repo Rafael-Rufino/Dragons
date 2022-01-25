3678f6813ba474a0795ee62ff2250a24ae6cfa69
RafaelRufino <rafaelrufino.52@gmail.com>
2022-01-25 01:09:22 -0300

    criando contexto de usuario

---
+++ src/contexts/user-context-provider.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import Context from "./use-context";
+const IsLoginPage = () => window.location.href.indexOf("login") !== -1;
+
+function UserContextProvider({ children }) {
+  let history = useHistory();
+  //const [user, setUser] = useState(null);
+
+  const [user, setUser] = useState(() => {
+    const storagedDragons = localStorage.getItem("@GithubDragons:user");
+    if (storagedDragons) {
+      return JSON.parse(storagedDragons);
+    }
+  });
+
+  useEffect(() => {
+    localStorage.setItem("@GithubDragons:user", JSON.stringify(user));
+  }, [user]);
+
+  function login({ user, password }) {
+    if (user === "Rafael" && password === "123456") {
+      setUser({ email: "user", name: "Rafael Rufino" });
+      history.push("/");
+    } else {
+      alert("usuario e senha invalida!");
+    }
+  }
+
+  function logout() {
+    setUser(null);
+  }
+
+  useEffect(() => {
+    if (!user && !IsLoginPage()) {
+      history.push("/login");
+    }
+  });
+
+  return (
+    <Context.Provider value={{ login, logout, user }}>
+      {children}
+    </Context.Provider>
+  );
+}
+
+export default UserContextProvider;
